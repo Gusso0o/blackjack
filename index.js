5238bf641cc1168e1b94f77b0ea7fc53517cb025
Marco <73793709+Gusso0o@users.noreply.github.com>
2023-04-14 20:12:24 +0200

    init

---
+++ index.js
@@ -0,0 +1,39 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+const createWindow = () => {
+
+  const mainWindow = new BrowserWindow({
+    width: 1100,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+};
+
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
